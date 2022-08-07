d075e2e64de034c76ce4bf13e0f9855f47e592b8
Uladzimir <vl.barbikov@gmail.com>
2022-08-07 17:48:34 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function toReadable (number) {
-  
-}
+    number = String(number);
+
+    const singleNumbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven','eight', 'nine', 
+                           'ten', 'eleven', 'twelve','thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+
+    const doubleNumbers = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    if (number.length === 1) {
+        return singleNumbers[number];
+    } else if (number.length === 2 && Number(number) < 20) {
+        return singleNumbers[number];
+    } else if (number.length === 2 && Number(number) >= 20) {
+        if (number % 10 === 0) {
+            return doubleNumbers[number[0] - 2];
+        } else {
+        return `${doubleNumbers[number[0] - 2]} ${singleNumbers[number[1]]}`;
+        }
+    } else if (number.length === 3){
+        if (number % 100 === 0) {
+            return `${singleNumbers[number[0]]} hundred`;
+        } else if (number % 10 === 0 && Number(number[1]) !== 1) {
+            return `${singleNumbers[number[0]]} hundred ${doubleNumbers[number[1] - 2]}`;
+        } else if (Number(number[1]) === 0) {
+            return `${singleNumbers[number[0]]} hundred ${singleNumbers[number[2]]}`;
+        } else if (Number(number[1]) === 1) {
+            return `${singleNumbers[number[0]]} hundred ${singleNumbers[Number(number.slice(1))]}`;
+        } else {
+            return `${singleNumbers[number[0]]} hundred ${doubleNumbers[number[1] - 2]} ${singleNumbers[number[2]]}`;
+        }
+    }
+};
+
